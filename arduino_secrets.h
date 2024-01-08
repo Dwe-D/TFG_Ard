9c4295beca248476e1a362f16bb896da6be4e64e
Dwe <91595242+Dwe-D@users.noreply.github.com>
2024-01-08 20:14:27 +0100

    *

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -1,5 +1,3 @@
 // Copy these keys from The Things Network console and paste them here:
-const char *appEui = "0000000000000001";
-const char *appKey = "4F247F08F7D9A7533CC693FB4CD0D183";
-
-
+const char *appEui = "****************";
+const char *appKey = "********************************";
