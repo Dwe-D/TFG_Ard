5bbb959d6ae253f1fa147b387bd23edb42ee4acc
Dwe <91595242+Dwe-D@users.noreply.github.com>
2024-02-04 01:20:45 +0100

    Update R0.ino

--- R0.ino
+++ R0.ino
@@ -1,22 +1,23 @@
 void setup() {
-  Serial.begin(9600); //Baud rate 
+  Serial.begin(9600);       
 }
  
 void loop() { 
-  float sensor_volt; //Define variable for sensor voltage 
-  float RS_air; //Define variable for sensor resistance
-  float R0; //Define variable for R0
-  float sensorValue=0; //Define variable for analog readings 
-  for(int x = 0 ; x < 500 ; x++) //Start for loop 
-  {
-    sensorValue = sensorValue + analogRead(A0); //Add analog values of sensor 500 times 
+  float sensor_volt;           
+  float RS_air;               
+  float R0;                   
+  float sensorValue=0;
+
+  for(int x = 0 ; x < 500 ; x++){
+    sensorValue = sensorValue + analogRead(A0);  
   }
-  sensorValue = sensorValue/500.0; //Take average of readings
-  sensor_volt = sensorValue*(5.0/1023.0); //Convert average to voltage 
-  RS_air = ((5.0*10.0)/sensor_volt)-10.0; //Calculate RS in fresh air 
-  R0 = RS_air/3.6; //Calculate R0 
+
+  sensorValue = sensorValue/500.0;          //Hacemos la media tras el bucle
+  sensor_volt = sensorValue*(5.0/1023.0);   //Lo convertinos a voltaje
+  RS_air = ((5.0*10.0)/sensor_volt)-10.0;   //Dada en el datasheet
+  R0 = RS_air/3.6;                          //Calculamos el valor de R0 
   sensorValue=0;
-  Serial.print("R0 = "); //Display "R0"
-  Serial.println(R0); //Display value of R0 
-  delay(1000); //Wait 1 second 
+  Serial.print("R0 = "); 
+  Serial.println(R0);  
+  delay(1000); 
 }
