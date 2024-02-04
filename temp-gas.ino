869451308d9b605d4d08d54473d6d0613fefa769
Dwe <91595242+Dwe-D@users.noreply.github.com>
2024-02-04 02:26:46 +0100

    Update temp-gas.ino

--- temp-gas.ino
+++ temp-gas.ino
@@ -2,13 +2,16 @@
 #include "arduino_secrets.h" 
 #include <DHT.h>
 
-#define DHTPIN 2  // Pin de datos del sensor DHT-11
-#define DHTTYPE DHT11  // Tipo de sensor DHT (DHT11 en este caso)
-#define BANDA EU868 // Define la banda de frecuencia utilizada 868 MHz 
-#define GASPIN A0 // Pin del sensor de gas
-#define M -0.263   // Pendiente
-#define B 0.42     // Intersección en Y
-#define R0 1.90    // Resistencia del sensor en aire fresco
+#define DHTPIN 2        // Pin de datos del sensor DHT-11
+#define DHTTYPE DHT11   // Tipo de sensor DHT 
+#define BANDA EU868     // Define la banda de frecuencia utilizada 868 MHz 
+#define GASPIN A0       // Pin del sensor de gas
+#define M -0.263        //Pendiente
+#define B 0.42          //Ordenada 
+#define R0 3.5          //Resistencia del sensor en aire fresco
+
+#define minutos 1       // Cada cuanto mandamos informacion
+const long periodo = minutos*60*1000;
 
 LoRaModem modem(Serial1);
 DHT dht(DHTPIN, DHTTYPE);
@@ -20,22 +23,37 @@ void setup() {
 }
 
 void loop() {
+  //Lectura de temperatura y humedad
   float temperatura = dht.readTemperature();
   float humedad = dht.readHumidity();
   int temp = temperatura * 100;
   int hum = humedad * 100;
 
-  float sensorValue = analogRead(GASPIN); // Cambiado gas_sensor a GASPIN
+  //Lectura del sensor de gases
+  float sensorValue = analogRead(GASPIN);
   float sensor_volt = sensorValue * (5.0 / 1023.0); 
   float RS_gas = ((5.0 * 10.0) / sensor_volt) - 10.0; 
   float ratio = RS_gas / R0; 
   double ppm_log = (log10(ratio) - B) / M; 
   double ppm_double = pow(10, ppm_log);
   int ppm = ppm_double * 100;
-  int lemo = 0;
 
+  //Umbral a partir del cual decimos que hemos detectado limoneno
+  int lemo = 0;
   if (ppm > 100) {
-    lemo = 1;  // Agregada coma al final
+    lemo = 1; 
+    byte detec[7];
+    detec[0] = temp >> 8;
+    detec[1] = temp;
+    detec[2] = hum >> 8;
+    detec[3] = hum;
+    detec[4] = ppm >> 8;
+    detec[5] = ppm;
+    detec[6] = lemo;
+
+    modem.beginPacket();
+    modem.write(detec, sizeof(byte) * 7);
+    modem.endPacket();
   }
 
   byte bytes[7];
@@ -47,13 +65,15 @@ void loop() {
   bytes[5] = ppm;
   bytes[6] = lemo;
 
+  //Envio a TTN
   modem.beginPacket();
   modem.write(bytes, sizeof(byte) * 7);
   modem.endPacket();
 
-  delay(10000);  // Cambiado de 1000 a 10000 para esperar 10 segundos
+  delay(periodo);  
 }
 
+//Conexion con TTN
 void connect() {
   if (!modem.begin(BANDA)) {
     Serial.println("Error al iniciar el modulo");
